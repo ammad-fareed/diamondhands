8b795ec6c4c0230c9e146e2167299653928e8a70
prio101 <miprio101@gmail.com>
2021-06-11 13:25:35 +0600

    update 1

--- tests/baked_page.spec.js
+++ tests/baked_page.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:3000/baked');  
+  await page.goto('http://localhost:3000/baked', {"timeout": 60000});  
   const name = await page.innerText('.Baked__InnerHeader-xn1e9l-4');
   const text = "🧁 Top tracks"
   expect(name).toBe(text);
